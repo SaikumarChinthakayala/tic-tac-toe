1768d0a7011799fe60380cb4df18250d7d9b8b14
SaikumarChinthakayala <saikumar.sssihl@gmail.com>
2024-09-06 00:35:03 +0300

    Identifies neighbours perfectly

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 import numpy as np
-#fields=np.array([x for x in range(0,3)], [y for y in range(0,3)])
-fields=np.array([(x,y) for x in range(0,3) for y in range(0,3)])
-print(fields[8])
-a=np.empty((3,3))
-print(a)
-print(a[0,1])
-
 """
 This game will have a board of 3x3 which is called a field and will 9 cells. 
 """
 class game():
     def __init__(self):
-        self.fields=np.empty([(x,y) for x in range(0,3) for y in range(0,3)])
+        self.fields=np.empty((3,3))
 
-
-    def neighbours(self, index):
-        picked_cell=self.fields(index)
+    @staticmethod
+    def NeighbourIndices(index):
+        if not isinstance(index, tuple):
+            raise Exception("Index needs to be a tuple")
+        return [(x,y) for x in range(index[0]-1, index[0]+2) for y in range(index[1]-1, index[1]+2) if (x,y) != index if ({x,y}).issubset({0,1,2})]
+        
     
 
+if __name__ == "__main__":
+    g=game()
+    #print(g.fields)
+    print(g.NeighbourIndices((2,2)))
